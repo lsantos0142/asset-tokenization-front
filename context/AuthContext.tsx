e38854b3f3f6e69e743c3ad61c360d7045d7cb3c
Lucas <lucasnego0142@gmail.com>
2022-10-08 04:32:32 -0300

    WIP: refresh

--- context/AuthContext.tsx
+++ context/AuthContext.tsx
@@ -2,19 +2,14 @@ import axios from "axios";
 import jwtDecode from "jwt-decode";
 import { createContext, useEffect, useState } from "react";
 
-interface AuthContextProps {
-    login: (user: any, authTokens: any) => void;
-    logout: () => void;
-    user: any;
-}
-
-const AuthContext = createContext({} as AuthContextProps);
+const AuthContext = createContext({} as any);
 
 export default AuthContext;
 
 export const AuthProvider = ({ children }: any) => {
     const [user, setUser] = useState<any>();
     const [authTokens, setAuthTokens] = useState<any>();
+    const [loading, setLoading] = useState<boolean>(true);
 
     useEffect(() => {
         if (localStorage.getItem("TOKENS")) {
@@ -27,6 +22,15 @@ export const AuthProvider = ({ children }: any) => {
             setAuthTokens(JSON.parse(localStorage.getItem("TOKENS") as string));
         }
     }, []);
+
+    useEffect(() => {
+        let interval = setInterval(() => {
+            if (authTokens) {
+                refresh();
+            }
+        }, 1000 * 60 * 5);
+        return () => clearInterval(interval);
+    }, [authTokens, loading]);
 
     const login = (user: any, authTokens: any) => {
         setUser(user);
@@ -53,10 +57,31 @@ export const AuthProvider = ({ children }: any) => {
             });
     };
 
+    const refresh = () => {
+        const headers = {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${authTokens.refreshToken}`,
+        };
+
+        axios
+            .post(`${process.env.BACK}/auth/refresh`, {}, { headers: headers })
+            .then((res) => {
+                if (res.status === 200) {
+                    setUser(jwtDecode(res.data.accessToken));
+                    setAuthTokens(res.data);
+                    localStorage.setItem("TOKENS", JSON.stringify(res.data));
+                }
+            })
+            .catch((e) => {
+                logout();
+            });
+    };
+
     let contextData = {
         user,
         login,
         logout,
+        refresh,
     };
     return (
         <AuthContext.Provider value={contextData}>
--- pages/user/index.tsx
+++ pages/user/index.tsx
@@ -7,7 +7,7 @@ import AuthContext from "../../context/AuthContext";
 const User: NextPage = () => {
     const router = useRouter();
 
-    const { user, logout } = useContext(AuthContext);
+    const { user, logout, refresh } = useContext(AuthContext);
 
     return (
         <>
@@ -17,6 +17,14 @@ const User: NextPage = () => {
                 }}
             >
                 Logout
+            </Button>
+
+            <Button
+                onClick={() => {
+                    refresh();
+                }}
+            >
+                Refresh
             </Button>
         </>
     );
