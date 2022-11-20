070e89aeb71a086ddc93e6e888dff6e9bc3d046f
Lucas <lucasnego0142@gmail.com>
2022-11-20 19:21:24 -0300

    fix env again

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    env: {
-        BASE_URL: "http://localhost:3000",
-        NEXT_PUBLIC_BACK: "http://localhost:4000",
-    },
     async redirects() {
         return [
             {
